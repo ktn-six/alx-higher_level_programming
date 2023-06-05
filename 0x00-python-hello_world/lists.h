671155df1f26ed995eb4d965bc63e2235b7a0020
ktn-six <tk6.ngeno@gmail.com>
2023-06-05 17:34:06 +0300

    hello world

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,6 +3,15 @@
 
 #include <stdlib.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
@@ -13,5 +22,5 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif 
+#endif /* LISTS_H */
 
